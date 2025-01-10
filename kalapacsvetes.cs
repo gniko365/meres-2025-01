b239b13324de7b6cc8121dccd06bb5e35c6221dc
gniko365 <gazsiniko18@gmail.com>
2025-01-10 09:43:22 +0100

    c sharp #2. feladat

---
+++ kalapacsvetes.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+class Sportolo
+{
+    public int Helyezes { get; set; }
+    public double Eredmeny { get; set; }
+    public string Nev { get; set; }
+    public string Orszagkod { get; set; }
+    public string Helyszin { get; set; }
+    public string Datum { get; set; }
+
+}
