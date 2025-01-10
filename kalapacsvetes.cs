94554d7e401d37ba9c1cec4553f1796495db5609
gniko365 <gazsiniko18@gmail.com>
2025-01-10 09:50:27 +0100

    konstruktor

--- kalapacsvetes.cs
+++ kalapacsvetes.cs
@@ -13,4 +13,16 @@ class Sportolo
     public string Helyszin { get; set; }
     public string Datum { get; set; }
 
+
+public Sportolo(string helyezes, string eredmney, sring nev, string orszagkod, string helyszin, string datum)
+{
+    Helyezes = int.Parse(helyezes);
+    Eredmeny = double.Parse (eredmeny, CultureInfo.InvariantCulture);
+    Nev = nev;
+    Orszagkod = orszagkod;
+    Helyszin = helyszin ;
+    Datum = datum;
 }
+
+}
+
--- kalapacsvetes.py
+++
@@ -1,9 +0,0 @@
-# 2. Feladat
-class Sportolo:
-    def __init__(self, helyezes, eredmeny, nev, orszagkod, helyszin, datum):
-        self.helyezes = int(helyezes)
-        self.eredmeny = float(eredmeny)
-        self.nev = nev
-        self.orszagkod = orszagkod
-        self.helyszin = helyszin
-        self.datum = datum
